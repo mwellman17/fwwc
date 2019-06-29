3f0ac5d20079b25a785419820cba6530439837d7
Michael Wellman <michaelrwellman@gmail.com>
2019-06-29 14:40:56 +0200

    sort score and name

--- app/controllers/homes_controller.rb
+++ app/controllers/homes_controller.rb
@@ -1,8 +1,8 @@
 class HomesController < ApplicationController
 
   def index
-    @production_brackets = Bracket.where(:in_pool => true).sort_by { |a| [!a.score, a.name] }
-    @talent_brackets = Bracket.where(:in_pool => false).sort_by { |a| [!a.score, a.name] }
+    @production_brackets = Bracket.where(:in_pool => true).sort_by { |a| [-a.score, a.name] }
+    @talent_brackets = Bracket.where(:in_pool => false).sort_by { |a| [-a.score, a.name] }
   end
 
 end
