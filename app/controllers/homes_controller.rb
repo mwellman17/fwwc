6eab27c5cbe37652825cb5c1893ad2daab649a16
Michael Wellman <michaelrwellman@gmail.com>
2019-06-30 10:10:52 +0200

    seeding and admin page

--- app/controllers/homes_controller.rb
+++ app/controllers/homes_controller.rb
@@ -1,9 +1,14 @@
 class HomesController < ApplicationController
+  before_action :authenticate_user!, except: [:index]
 
   def index
     $scorecard = Scorecard.first
     @production_brackets = Bracket.where(:in_pool => true).sort_by { |a| [-a.score, a.last_name] }
     @talent_brackets = Bracket.where(:in_pool => false).sort_by { |a| [-a.score, a.last_name] }
+  end
+
+  def admin
+    @scorecard = Scorecard.first
   end
 
 end
