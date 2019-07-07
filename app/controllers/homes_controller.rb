701b0801df9b8699634aed054f258fa1c7968dbd
Michael Wellman <michaelrwellman@gmail.com>
2019-07-07 19:12:41 +0200

    fixed talent scoring

--- app/controllers/homes_controller.rb
+++ app/controllers/homes_controller.rb
@@ -4,7 +4,7 @@ class HomesController < ApplicationController
   def index
     $scorecard = Scorecard.first
     @production_brackets = Bracket.where(:in_pool => true).sort_by { |a| [-a.score, a.last_name] }
-    @talent_brackets = Bracket.where(:in_pool => false).sort_by { |a| [-a.score, a.last_name] }
+    @talent_brackets = Bracket.where(:in_pool => false).sort_by { |a| [-a.talent_score, a.last_name] }
   end
 
   def admin
